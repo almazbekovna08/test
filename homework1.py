707286ad99f0c8ecc6fd8e40134519d266e97cdc
almazbekovna08 <themoongirl234@gmail.com>
2024-10-24 18:15:32 +0600

    я сделала доп задание

---
+++ homework1.py
@@ -0,0 +1,17 @@
+"Task 1"
+print("ABOUT ME")
+print("Nuraiym, 16 years old, study at Taylor, learn different languages.")
+print("I like listen to music, drink coffee with milk ahd eat different desserts.")
+print("""If tell about my regular days...
+      I get up at 9 o'clock every day, have breakfast,halp my mother with clean in home,
+       getting dressed and go to the city to Taylor at 12 o'clock, then after Taylor I have lunch and go to the Geeks
+       for lesson at 4 o'clock, after lessons I go to home, have dinner and to clean home again, then do my homeworks ahd go to the bad.""")
+"Task 2"
+print("7+7 =", 7+7)
+print("6+6 =", 6+6)
+print("3*3 =", 3*3)
+print("5-1 =", 5-1)
+print("9+1 =", 9+1)
+print("5-2 =", 5-2)
+print("6*9 =", 6*9)
+print("9/3 =", 9/3)
